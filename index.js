073e0c97187c544bf36e46a502471fba709c864c
Hritik Gupta <106511117+HritikGupta22@users.noreply.github.com>
2024-01-11 03:18:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 });
 
-    
-console.log(menuIcon);
-
-
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
